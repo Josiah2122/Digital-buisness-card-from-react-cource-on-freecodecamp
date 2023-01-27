443077c9975d1de8ea7c8337301308072bbea46a
Josiah2122 <98037818+Josiah2122@users.noreply.github.com>
2023-01-27 15:30:27 +0300

    Create Interests.js

---
+++ Interests.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Interests(){
+ return(
+  <div className="main-content">
+   <h1>Interests</h1>
+   <p>I like watching TikTok, Youtube, Movies, Games, reading Fictions, self-help book, Novel </p>
+  </div>
+ )
+}
